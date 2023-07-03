83d2f2b4fe436835913dc5411b7caa12033ed66b
kelvin27315 <seven_units@yahoo.co.jp>
2023-07-04 04:08:28 +0900

    M PLUS 1Pフォントの有効化

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,9 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    fontFamily: {
+      sans: ["M PLUS 1P"]
+    }
+  }
 } as Options;
