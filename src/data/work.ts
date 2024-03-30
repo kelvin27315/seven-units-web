64d34142ad72ac6d6aa8c1ef24f7ed06108cd418
kelvin27315 <development@none.cloud>
2024-03-30 19:11:00 +0900

    画像をgitで管理するのではなくCloudflare R2から配信する

--- src/data/work.ts
+++ src/data/work.ts
@@ -1,7 +1,7 @@
 export type Work = {
   slug: string;
   title: string;
-  imageName: string;
+  imageURL: URL;
   date: Date;
   pixivURL: URL;
 };
@@ -10,21 +10,23 @@ export const works: Work[] = [
   {
     slug: "q",
     title: "Q",
-    imageName: "q.jpg",
+    imageURL: new URL("https://images.seven-units.net/q.jpg"),
     date: new Date(2019, 11, 17),
     pixivURL: new URL("https://www.pixiv.net/artworks/77813238"),
   },
   {
     slug: "thinking_akyu_keycap",
     title: "Thinking_Akyu_keycap",
-    imageName: "thinking_akyu_keycap.jpg",
+    imageURL: new URL(
+      "https://images.seven-units.net/thinking_akyu_keycap.jpg",
+    ),
     date: new Date(2021, 7, 18),
     pixivURL: new URL("https://www.pixiv.net/artworks/73461733"),
   },
   {
     slug: "thinking_akyu",
     title: "Thinking_Akyu",
-    imageName: "thinking_akyu.jpg",
+    imageURL: new URL("https://images.seven-units.net/thinking_akyu.jpg"),
     date: new Date(2019, 3, 10),
     pixivURL: new URL("https://www.pixiv.net/artworks/73461733"),
   },
